c6675bf599e89c5198a595b795ce13c0d80df075
hanbing <hblx2017@163.com>
2022-06-29 17:38:41 +0800

    test: 测试侧边栏配置项

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,6 +23,30 @@ module.exports = {
             },
             { text: '构建工具', link: '/pages/buildtools/index.md' },
             { text: 'Github', link: 'https://github.com/iJay' },
-        ]
+        ],
+        sidebar: {
+            // docs文件夹下面的/pages/browser/文件夹 文档中md文件 书写的位置(命名随意)
+            '/pages/browser/': [
+                '/pages/browser/', // /pages/browser/文件夹的README.md 不是下拉框形式
+                {
+                    title: '侧边栏下拉框的标题1',
+                    children: [
+                        '/pages/browser/render', // 以docs为根目录来查找文件 
+                        // 上面地址查找的是：docs>pages>browser>render.md 文件
+                        // 自动加.md 每个子选项的标题 是该md文件中的第一个h1/h2/h3标题
+                    ]
+                }
+            ],
+            // docs文件夹下面的algorithm文件夹 这是第二组侧边栏 跟第一组侧边栏没关系
+            '/pages/algorithm/': [
+                '/algorithm/', 
+                {
+                    title: '第二组侧边栏下拉框的标题1',
+                    children: [
+                      '/algorithm/simple/test' 
+                    ]
+                }
+            ]
+        }
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -4,6 +4,6 @@ heroImage: /home-pic.jpeg
 heroText: 记录前端路上的点滴
 tagline: 日拱一卒，功不唐捐
 actionText: 进去瞅瞅 →
-actionLink: /pages/folder1/test1.md
+actionLink: /pages/browser/index.md
 footer: MIT Licensed | Copyright © 2022-present LeoHan
 ---
